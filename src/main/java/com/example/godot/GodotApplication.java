7dd7b88e8891cedf6e18347b3dfdfecd5bee87b1
LiuYang <lygithub@163.com>
2024-02-06 17:21:09 +0800

    doc

---
+++ src/main/java/com/example/godot/GodotApplication.java
@@ -0,0 +1,13 @@
+package com.example.godot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodotApplication.class, args);
+    }
+
+}
